a34ee98a944a655423c4301b1ea1616668331ead
Oliver Tworkowski <olitworkowski@gmail.com>
2023-04-14 18:50:18 +0200

    implement faster accessed bookmark bar in dashboard header, improve identicon avatar component

--- webapp/src/components/identicon_avatar/index.jsx
+++ webapp/src/components/identicon_avatar/index.jsx
@@ -5,18 +5,19 @@ import "./style.scss";
 import { useMemo } from "react";
 
 const IdenticonAvatar = (props) => {
-  const { id } = props;
+  const { id, ...rest} = props;
 
   const identiconImage = useMemo(() => {
+    if (id === undefined || id === null) return null;
     try {
-      const data = new Identicon(id, 420).toString();
+      const data = new Identicon(id.toString(), 128).toString();
       return `data:image/png;base64,${data}`;
     } catch (error) {
       return null;
     }
   }, [id]);
 
-  return <Avatar src={identiconImage} />;
+  return <Avatar src={identiconImage} {...rest} />;
 };
 
 export default IdenticonAvatar;
