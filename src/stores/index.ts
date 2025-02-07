23e9825cd5b3945bf621566a4a5785966295f48f
tworkool <olitworkowski@gmail.com>
2025-02-07 11:57:10 +0100

    fix small value paper visual issue in grids, change default value for settings

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -16,7 +16,7 @@ const defaultSettings: ISettings = {
   automaticUpdateInterval: 60,
   boxInactiveAfter: 24,
   sensorInactiveAfter: 12,
-  fallbackNullValue: "<EMPTY>",
+  fallbackNullValue: "N/A",
   primaryDashboardColor: "#9038e8",
   dateFormat: "MMM Do YY",
 };
